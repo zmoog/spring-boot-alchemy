9b82be527c59134ce59abb0a44be4cf20c694d58
Maurizio Branca <mbranca@topcon.com>
2015-08-07 18:29:49 +0200

    Initial import

---
+++ src/main/java/alchemy/AlchemyApplication.java
@@ -0,0 +1,12 @@
+package alchemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlchemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlchemyApplication.class, args);
+    }
+}
