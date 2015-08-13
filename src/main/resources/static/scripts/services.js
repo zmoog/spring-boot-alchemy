1ca2b927eef7a65ee2bfe0a27735102804d6579c
Maurizio Branca <mbranca@topcon.com>
2015-08-13 19:21:29 +0200

    Format improvement.

--- src/main/resources/static/scripts/services.js
+++ src/main/resources/static/scripts/services.js
@@ -1,15 +1,17 @@
 (function() {
 
 	angular.module('alchemy')
+
 		.factory('AccountService', ['$http', function($http){
-
-			// var accounts = {};
-
 			return {
 				list: function() {
 					return $http.get('/api/accounts.json');
 				}
 			};
-		}])
+		}]);
 
 })();
+
+
+
+
